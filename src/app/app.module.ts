37c2fd1b0c2751c1bb6f1be64741b1b31b6344e8
nandaditra <nandadditra56@gmail.com>
2023-10-30 11:58:39 +0700

    migrating shared component to layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { KarirComponent } from './modules/karir/karir.component';
 import { NotFoundComponent } from './modules/not-found/not-found.component';
 import { SitusKamiComponent } from './modules/situs-kami/situs-kami.component';
 import { AppRoutingModule } from '../app-routing.module';
-import { HeaderHomeComponent } from './shared/header-home/header-home.component';
-import { HeaderWebComponent } from './shared/header-web/header-web.component';
-import { FooterHomeComponent } from './shared/footer-home/footer-home.component';
-import { FooterWebComponent } from './shared/footer-web/footer-web.component';
+import { HeaderHomeComponent } from './layout/header-home/header-home.component';
+import { HeaderWebComponent } from './layout/header-web/header-web.component';
+import { FooterHomeComponent } from './layout/footer-home/footer-home.component';
+import { FooterWebComponent } from './layout/footer-web/footer-web.component';
 
 @NgModule({
   declarations: [
