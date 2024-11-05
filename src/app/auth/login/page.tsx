47b53f5000dfaab5cf1154fe31bb48241ad4f64b
nandaditra <nandaditra56@gmail.com>
2024-11-05 19:36:57 +0700

    add some background on auth

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -21,10 +21,10 @@ export default function Login() {
         <div>
             <Header />
 
-            <main>
-                <div className="container mx-auto px-4 md:px-36">
-                    <h1 className="text-green-black text-3xl text-center font-bold">Login</h1>
-                    <form className="p-6 md:p-10 my-5 mx-auto md:w-2/5 border border-1 rounded-md">
+            <main className="bg-[url('@/assets/img/auth/login.jpg')] bg-cover bg-no-repeat">
+                <div className="container mx-auto px-4 py-24 md:px-36">
+                    <form className="p-6 md:p-10 my-5 ml-auto md:w-2/5 border border-1 rounded-md bg-white">
+                        <h1 className="text-green-black text-3xl text-center font-bold">Login</h1>
                         <div className="my-2">
                             <label className="text-green-black">email</label><br/>
                             <input className="w-full p-2 rounded-md border border-2 focus:border-green-black" name="email"/>
