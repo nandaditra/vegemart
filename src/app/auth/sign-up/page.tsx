82d361ab580759faa50eb49ce5de2d656759d064
nandaditra <nandaditra56@gmail.com>
2024-11-05 19:46:04 +0700

    add background

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -19,10 +19,10 @@ export default function SignUp() {
         <div>
             <Header />
 
-            <main>
-                <div className="container mx-auto px-4 md:px-36">
-                        <h1 className="text-green-black text-3xl text-center font-bold">Daftar</h1>
-                        <form className="p-6 md:p-10 my-5 mx-auto md:w-2/5 border border-1 rounded-md">
+            <main className="bg-[url('@/assets/img/auth/signup.jpg')] bg-cover bg-no-repeat">
+                <div className="container mx-auto px-4 py-20 md:px-36"> 
+                        <form className="p-6 md:p-10 ml-auto md:w-2/5 border border-1 rounded-md bg-white">
+                            <h1 className="text-green-black text-3xl text-center font-bold">Daftar</h1>
                             <div className="my-2">
                                 <label className="text-green-black">name</label><br/>
                                 <input className="w-full p-2 rounded-md border border-2 focus:border-green-black" name="name"/>
